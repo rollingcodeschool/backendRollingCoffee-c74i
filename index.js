7cf6a8d1c21a662bf058a79d3dcc1f899af1cd7b
Emilse Arias <emilsearias@gmail.com>
2024-03-05 11:35:59 -0300

    agregamos el resto de los middlewares y un ejemplo de solicitud get

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan';
+import 'dotenv/config'; //permite procesar variables de entorno
 // node --watch index.js comando experimental para desarrollo
 console.log('bienvenidos c74i')
 
@@ -10,5 +13,13 @@ app.listen(app.get('port'), ()=>{
     console.log('Estoy en el puerto '+app.get('port'));
 })
 // 2- configurar middlewares
+app.use(cors()); //permite conexiones remotas
+app.use(morgan('dev')) //muestra informacion extra en la terminal
+app.use(express.json()) //permite interpretar el formato json
+app.use(express.urlencoded({extended:true})) //me permite interpretar los datos del body de un request
 
 // 3- configuracion de las rutas
+app.get('/', (req, res)=>{
+ console.log('hola mundo');
+ res.send('desde el backend de rollingCoffee');
+})
