ba7abb9feecbaeb6906ebfe5e423dab142313184
Emilse Arias <emilsearias@gmail.com>
2024-03-05 11:15:59 -0300

    configuramos un puerto para el repo

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ import express from 'express'
 console.log('bienvenidos c74i')
 
 // 1- configurar un puerto
+const app = express();
 
+app.set('port', process.env.PORT || 4000 )
+app.listen(app.get('port'), ()=>{
+    console.log('Estoy en el puerto '+app.get('port'));
+})
 // 2- configurar middlewares
 
 // 3- configuracion de las rutas
