6bd4954dfe41673e341cd80deb7f9cb410c9c2a7
Emilse Arias <emilsearias@gmail.com>
2024-03-07 10:06:00 -0300

    agregamos el index.html como archivo estatico

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan';
 import 'dotenv/config'; //permite procesar variables de entorno
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 // node --watch index.js comando experimental para desarrollo
 console.log('bienvenidos c74i')
 
@@ -14,12 +17,18 @@ app.listen(app.get('port'), ()=>{
 })
 // 2- configurar middlewares
 app.use(cors()); //permite conexiones remotas
-app.use(morgan('dev')) //muestra informacion extra en la terminal
+app.use(morgan('dev')) //muestra información extra en la terminal
 app.use(express.json()) //permite interpretar el formato json
 app.use(express.urlencoded({extended:true})) //me permite interpretar los datos del body de un request
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// console.log(__filename)
+// console.log(path.join(__dirname,'/public'))
+app.use(express.static(path.join(__dirname,'/public')));
 
 // 3- configuracion de las rutas
-app.get('/', (req, res)=>{
+app.get('/nuevo', (req, res)=>{
  console.log('hola mundo');
  res.send('desde el backend de rollingCoffee');
-})
+});
+
